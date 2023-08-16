997387a03d0d5f07b5707a187bb6c614370f753b
Author Name <47639983+JustTemmie@users.noreply.github.com>
2023-08-16 05:50:38 +0200

    a

--- API/main.py
+++ API/main.py
@@ -67,7 +67,7 @@ def download_video(video_id):
         ydl.download(f"https://www.youtube.com/watch?v={video_id}")
             
     print("---------------------")
-    return send_file(fileLocation, as_attachment=True, download_name="video")
+    return send_file(fileLocation, as_attachment=True)
     
     
     # file = read_image(pid)
